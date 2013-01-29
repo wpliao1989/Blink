93b2c6d8e7f357cabd32db2f5b7001b6516823c5
Wei Ping <User@Wei-Pings-Mac.local>
2013-01-29 09:41:53 +0800

    Initial Commit

---
+++ Blink/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Blink
+//
+//  Created by Wei Ping on 13/1/29.
+//  Copyright (c) 2013年 flyingman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
